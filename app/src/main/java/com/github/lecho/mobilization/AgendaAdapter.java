c80f555d9c44a5b9e5cae5b91789332197da206d
Leszek Wach <leszekwach@gmail.com>
2015-07-06 20:53:16 +0200

    Improved agenda item layout

--- app/src/main/java/com/github/lecho/mobilization/AgendaAdapter.java
+++ app/src/main/java/com/github/lecho/mobilization/AgendaAdapter.java
@@ -4,9 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class AgendaAdapter extends RecyclerView.Adapter<AgendaAdapter.AgendaViewHolder> {
