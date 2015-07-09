804bec9e5a98c06404b2e547c3635ab3a1856820
Leszek Wach <leszekwach@gmail.com>
2015-07-09 22:32:19 +0200

    Added burger icon for main activity

--- app/src/main/java/com/github/lecho/mobilization/MainActivity.java
+++ app/src/main/java/com/github/lecho/mobilization/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -26,7 +27,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         Menu navigationViewMenu = navigationView.getMenu();
 
         if (null == savedInstanceState) {
