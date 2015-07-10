33ad829de521a2c6e16664eda1dc12e1f16c43c6
Leszek Wach <leszekwach@gmail.com>
2015-07-10 23:27:53 +0200

    Partial UI for talk activity

--- app/src/main/java/com/github/lecho/mobilization/TalkActivity.java
+++ app/src/main/java/com/github/lecho/mobilization/TalkActivity.java
@@ -1,6 +1,8 @@
 package com.github.lecho.mobilization;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,25 +17,30 @@ public class TalkActivity extends AppCompatActivity {
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
+    @Bind(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsingToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talk);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("TALK");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        collapsingToolbar.setTitle("10:30 - 11:00");
+        collapsingToolbar.setExpandedTitleColor(Color.TRANSPARENT);
+        collapsingToolbar.setCollapsedTitleTextColor(Color.WHITE);
+
         if (null == savedInstanceState) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.content_container, TalkFragment.newInstance()).commit();
+            //FragmentManager fragmentManager = getSupportFragmentManager();
+            //fragmentManager.beginTransaction().replace(R.id.content_container, TalkFragment.newInstance()).commit();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_talk, menu);
         return true;
     }
 
