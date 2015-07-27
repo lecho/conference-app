f7be39bcc69bb8c91465190cbe24daf1f70a469d
Leszek Wach <leszekwach@gmail.com>
2015-07-27 22:36:31 +0200

    Added models with methods to desterilize from json

---
+++ app/src/main/java/com/github/lecho/mobilization/model/Speaker.java
@@ -0,0 +1,7 @@
+package com.github.lecho.mobilization.model;
+
+/**
+ * Created by Leszek on 2015-07-24.
+ */
+public class Speaker {
+}
--- app/src/main/java/com/github/lecho/mobilization/model/Talk.java
+++ app/src/main/java/com/github/lecho/mobilization/model/Talk.java
@@ -7,39 +7,44 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Created by Leszek on 2015-07-24.
  */
-public class Break {
+public class Talk {
 
     public String key;
     public String title;
     public String descriptionHtml;
+    public String[] speakersKeys;
+    public String language;
 
-    public static List<Break> fromJson(String json) {
-        Type genericType = new TypeToken<Map<String, Break>>() {
+    public static List<Talk> fromJson(String json) {
+        Type genericType = new TypeToken<Map<String, Talk>>() {
         }.getType();
 
         Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
-        Map<String, Break> breaksMap = gson.fromJson(json, genericType);
-        for (Map.Entry<String, Break> entry : breaksMap.entrySet()) {
+        Map<String, Talk> talksMap = gson.fromJson(json, genericType);
+        for (Map.Entry<String, Talk> entry : talksMap.entrySet()) {
             entry.getValue().key = entry.getKey();
         }
 
-        List<Break> breaks = new ArrayList<>(breaksMap.size());
-        breaks.addAll(breaksMap.values());
-        return breaks;
+        List<Talk> talks = new ArrayList<>(talksMap.size());
+        talks.addAll(talksMap.values());
+        return talks;
     }
 
     @Override
     public String toString() {
-        return "Break{" +
+        return "Talk{" +
                 "key='" + key + '\'' +
                 ", title='" + title + '\'' +
                 ", descriptionHtml='" + descriptionHtml + '\'' +
+                ", speakersKeys=" + Arrays.toString(speakersKeys) +
+                ", language='" + language + '\'' +
                 '}';
     }
 }
