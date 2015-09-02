a175022b55a4a3fbfd644e9178386c2e434fc9e7
Leszek Wach <leszekwach@gmail.com>
2015-09-02 20:00:53 +0200

    Add burger icon event to open navigation drawer

--- app/src/main/java/com/github/lecho/mobilization/ui/MainActivity.java
+++ app/src/main/java/com/github/lecho/mobilization/ui/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.navigation_view)
     NavigationView navigationView;
 
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,13 +52,11 @@ public class MainActivity extends AppCompatActivity {
             fragmentManager.beginTransaction().replace(R.id.content_container, MyAgendaFragment.newInstance()).commit();
         }
 
-        ApiData apiData = new ApiFacade().parseJsonFilesFromAssets(this, "test-data");
-
-        RealmFacade facade = new RealmFacade(this);
-        facade.saveData(apiData);
-
-        AgendaViewDto agendaViewDto = facade.loadWholeAgenda();
-        Log.e("TAG", "Agenda: " + agendaViewDto.toString());
+        //ApiData apiData = new ApiFacade().parseJsonFilesFromAssets(this, "test-data");
+        //RealmFacade facade = new RealmFacade(this);
+        //facade.saveData(apiData);
+        //AgendaViewDto agendaViewDto = facade.loadWholeAgenda();
+        //Log.e("TAG", "Agenda: " + agendaViewDto.toString());
     }
 
     @Override
@@ -69,24 +72,32 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, TalkActivity.class);
-            startActivity(intent);
-            return true;
-        } else if (id == R.id.action_settings2) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.content_container, VenueAgendaFragment.newInstance())
-                    .commit();
-            return true;
-        } else if (id == R.id.action_settings3) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.content_container, SponsorsFragment.newInstance()).commit();
-            return true;
-        } else if (id == R.id.action_settings4) {
-            Intent intent = new Intent(this, SpeakerActivity.class);
-            startActivity(intent);
-            return true;
+        switch (id) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings: {
+                Intent intent = new Intent(this, TalkActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            case R.id.action_settings2: {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_container, VenueAgendaFragment.newInstance())
+                        .commit();
+                return true;
+            }
+            case R.id.action_settings3: {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_container, SponsorsFragment.newInstance())
+                        .commit();
+                return true;
+            }
+            case R.id.action_settings4: {
+                Intent intent = new Intent(this, SpeakerActivity.class);
+                startActivity(intent);
+                return true;
+            }
         }
 
         return super.onOptionsItemSelected(item);
