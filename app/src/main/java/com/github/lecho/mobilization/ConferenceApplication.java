21dcf7486744eec9419c1ec8ecd926443382f640
Leszek Wach <leszekwach@gmail.com>
2016-07-09 19:05:48 +0200

    Update database and app versions

--- app/src/main/java/com/github/lecho/mobilization/ConferenceApplication.java
+++ app/src/main/java/com/github/lecho/mobilization/ConferenceApplication.java
@@ -13,7 +13,7 @@ public class ConferenceApplication extends Application {
     /**
      * Always increment schema version.
      */
-    private static final int SCHEMA_VERSION = 6;
+    private static final int SCHEMA_VERSION = 7;
 
     @Override
     public void onCreate() {
