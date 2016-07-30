192d7c932bb2339528aecbecdb7ed9577115c217
Leszek Wach <leszekwach@gmail.com>
2016-07-30 22:41:00 +0200

    Add EventFragment

---
+++ app/src/main/java/com/github/lecho/mobilization/ui/fragment/EventFragment.java
@@ -0,0 +1,22 @@
+package com.github.lecho.mobilization.ui.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.lecho.mobilization.R;
+
+public class EventFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_event, container, false);
+    }
+}
