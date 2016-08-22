8f1a25741862ce71e09a2dc10b7923ee0f137454
Leszek Wach <leszekwach@gmail.com>
2016-08-22 20:47:33 +0200

    Fixed FAB refresh after slot conflict positive reaction

--- app/src/main/java/com/github/lecho/mobilization/ui/loader/TalkLoader.java
+++ app/src/main/java/com/github/lecho/mobilization/ui/loader/TalkLoader.java
@@ -5,8 +5,12 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.github.lecho.mobilization.BuildConfig;
+import com.github.lecho.mobilization.rx.MyAgendaUpdatedEvent;
+import com.github.lecho.mobilization.rx.RxBus;
 import com.github.lecho.mobilization.util.Optional;
 import com.github.lecho.mobilization.viewmodel.TalkViewModel;
+
+import rx.Subscription;
 
 /**
  * Created by Leszek on 2015-09-03.
@@ -15,6 +19,7 @@ public class TalkLoader extends BaseRealmLoader<Optional<TalkViewModel>> {
 
     private static final String TAG = TalkLoader.class.getSimpleName();
     private String talkKey;
+    private Subscription agendaUpdateSubscription;
 
     public static TalkLoader getLoader(Context context, String talkKey) {
         return new TalkLoader(context, talkKey);
@@ -35,5 +40,21 @@ public class TalkLoader extends BaseRealmLoader<Optional<TalkViewModel>> {
         }
         Optional<TalkViewModel> newData = realmFacade.loadTalkByKey(talkKey);
         return newData;
+    }
+
+    protected void registerObserver() {
+        super.registerObserver();
+        if (agendaUpdateSubscription == null) {
+            agendaUpdateSubscription = RxBus.subscribe(MyAgendaUpdatedEvent.class, databaseUpdatedEvent ->
+                    TalkLoader.this.onContentChanged());
+        }
+    }
+
+    protected void unregisterObserver() {
+        super.unregisterObserver();
+        if (agendaUpdateSubscription != null) {
+            agendaUpdateSubscription.unsubscribe();
+            agendaUpdateSubscription = null;
+        }
     }
 }
