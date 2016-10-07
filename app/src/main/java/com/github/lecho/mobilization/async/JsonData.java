9699e5b5f159b1944730093895ccc8ac07dfea97
Leszek Wach <leszekwach@gmail.com>
2016-10-07 21:59:25 +0200

    [26] Add JsonData model for checking data version

---
+++ app/src/main/java/com/github/lecho/mobilization/async/JsonData.java
@@ -0,0 +1,13 @@
+package com.github.lecho.mobilization.async;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by Leszek on 07.10.2016.
+ */
+
+@IgnoreExtraProperties
+public class JsonData {
+
+    public long version;
+}
